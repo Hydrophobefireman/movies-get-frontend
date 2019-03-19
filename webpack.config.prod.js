5096dcb5bf07c3438e55f6f924feabe30617d9e5
Your Name <you@example.com>
2019-03-19 22:53:19 +0530

    increase.min support

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,9 +22,7 @@ module.exports = {
             presets: [
               [
                 "@babel/preset-env",
-                {
-                  targets: ">0.5%,not ie 11,not op_mini all"
-                }
+                
               ]
             ],
             plugins: [

18f121d989d38dfa58a0c77e152d4e81e37cce77
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2019-08-31 17:34:12 +0530

    fix some lighthouse problems

--- static/js/components/HeaderComponent/HeaderComponent.js
+++ static/js/components/HeaderComponent/HeaderComponent.js
@@ -58,6 +58,7 @@ export class HeaderComponent extends Component {
         "header",
         null,
         h("img", {
+          alt: "menu button",
           src:
             "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDBWMHoiPjwvcGF0aD48cGF0aCBkPSJNMyAxOGgxOHYtMkgzdjJ6bTAtNWgxOHYtMkgzdjJ6bTAtN3YyaDE4VjZIM3oiIGZpbGw9IndoaXRlIj48L3BhdGg+PC9zdmc+",
           style: {
